86ff249b7a08dc6ecd7e59874df253619d7fe038
Omar <otantawy53@gmail.com>
2022-05-01 15:53:49 -0400

    commit tree 672e5872d05be632626e6aac611338eaa03fc260

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+let main = document.querySelector('main');
+// console.log(main);
+
+main.remove();
+
+// let newHeader = document.createElement('h1#victory');
+let newHeader = document.createElement('h1');
+newHeader.id = ('victory');
+
+newHeader.textContent = ('Omar is the champion');
